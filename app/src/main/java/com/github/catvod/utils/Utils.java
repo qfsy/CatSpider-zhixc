22f65efb648e27e84eeff7c3896352f3ee459d3e
zhixc <eg79@qq.com>
2023-10-23 11:57:49 +0800

    引入gson和FongMi的CatVodSpider仓库的一些实体类

---
+++ app/src/main/java/com/github/catvod/utils/Utils.java
@@ -0,0 +1,5 @@
+package com.github.catvod.utils;
+
+public class Utils {
+    public static final String CHROME = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36";
+}
