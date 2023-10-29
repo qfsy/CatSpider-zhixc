60a4cd5942cc9d38ae0e17b4968829e2eb67519d
zhixc <eg79@qq.com>
2023-10-29 21:32:53 +0800

    更新 Utils 工具类

--- app/src/main/java/com/github/catvod/utils/Utils.java
+++ app/src/main/java/com/github/catvod/utils/Utils.java
@@ -2,4 +2,18 @@ package com.github.catvod.utils;
 
 public class Utils {
     public static final String CHROME = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36";
+
+
+    public static String substring(String text) {
+        return substring(text, 1);
+    }
+
+    public static String substring(String text, int num) {
+        if (text != null && text.length() > num) {
+            return text.substring(0, text.length() - num);
+        } else {
+            return text;
+        }
+    }
+
 }
